2bae23baac72c747a760e97bd4da0250cdee6846
“Jonathan <jonathan.valere@gmail.com>
2023-11-13 15:39:14 -0300

    modification du model Offer. Restructuration avec Schema et ajout de timestamps

--- models/Offer.js
+++ models/Offer.js
@@ -1,31 +1,38 @@
 const mongoose = require("mongoose");
 
-const Offer = mongoose.model("Offer", {
-  product_name: {
-    type: String,
-    // maxlength:50,
-    validate: {
-      validator: function (value) {
-        // Le titre de l'offre doit contenir moins de 50 caractères
-        return value.length <= 50;
+const offerSchema = mongoose.Schema(
+  {
+    product_name: {
+      type: String,
+      // maxlength:50,
+      validate: {
+        validator: function (value) {
+          // Le titre de l'offre doit contenir moins de 50 caractères
+          return value.length <= 50;
+        },
+        message: `Votre titre doit comporter moins de 50 caractères.`,
       },
-      message: `Votre titre doit comporter moins de 50 caractères.`,
+    },
+    product_description: {
+      type: String,
+      maxlength: 500,
+    },
+    product_price: {
+      type: Number,
+      max: 100000,
+    },
+    product_details: Array,
+    product_image: Object,
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
   },
-  product_description: {
-    type: String,
-    maxlength: 500,
-  },
-  product_price: {
-    type: Number,
-    max: 100000,
-  },
-  product_details: Array,
-  product_image: Object,
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Offer = mongoose.model("Offer", offerSchema);
 
 module.exports = Offer;
